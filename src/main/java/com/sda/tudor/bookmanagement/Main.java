211a15b9f2dcf707e672519266810beb149980ff
Mateuț Tudor-Vlad <tudormateut@yahoo.com>
2022-11-16 19:48:15 +0200

    BMS-16: Add Hibernate model.

--- src/main/java/com/sda/tudor/bookmanagement/Main.java
+++ src/main/java/com/sda/tudor/bookmanagement/Main.java
@@ -1,7 +1,9 @@
 package com.sda.tudor.bookmanagement;
 
+import com.sda.tudor.bookmanagement.utils.SessionManager;
+
 public class Main {
     public static void main(String[] args) {
-
+        SessionManager.getSessionFactory();
     }
 }
--- src/main/java/com/sda/tudor/bookmanagement/utils/SessionManager.java
+++ src/main/java/com/sda/tudor/bookmanagement/utils/SessionManager.java
@@ -1,5 +1,8 @@
 package com.sda.tudor.bookmanagement.utils;
 
+import com.sda.tudor.bookmanagement.model.Author;
+import com.sda.tudor.bookmanagement.model.Book;
+import com.sda.tudor.bookmanagement.model.BookReview;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -21,5 +24,8 @@ public class SessionManager extends AbstractSessionManager {
     @Override
     protected void setAnnotatedClasses(Configuration configuration) {
         // Hibernate model will be added here
+        configuration.addAnnotatedClass(Author.class);
+        configuration.addAnnotatedClass(Book.class);
+        configuration.addAnnotatedClass(BookReview.class);
     }
 }
