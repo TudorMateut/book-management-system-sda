ff9826abc8fc7045b8e76b27948697995e7a57db
Mateuț Tudor-Vlad <tudormateut@yahoo.com>
2022-11-16 20:56:35 +0200

    DONE - BMS-18: Create user menu.

--- src/main/java/com/sda/tudor/bookmanagement/Main.java
+++ src/main/java/com/sda/tudor/bookmanagement/Main.java
@@ -1,9 +1,44 @@
 package com.sda.tudor.bookmanagement;
 
+import com.sda.tudor.bookmanagement.menu.UserOption;
 import com.sda.tudor.bookmanagement.utils.SessionManager;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         SessionManager.getSessionFactory();
+        Scanner scanner = new Scanner(System.in);
+
+        UserOption userOption;
+
+        do {
+            UserOption.printAllOptions();
+            System.out.println("Please select an option!");
+            try {
+                int numericOption = Integer.parseInt(scanner.nextLine());
+                userOption = UserOption.findUserOption(numericOption);
+            } catch (NumberFormatException e) {
+                userOption = UserOption.UNKNOWN;
+            }
+
+            switch (userOption) {
+                case CREATE_AUTHOR:
+                    System.out.println("Not implemented!");
+                    break;
+                case EXIT:
+                    System.out.println("Goodbye!");
+                    break;
+                case UNKNOWN:
+                    System.out.println("Option unknown!");
+                    break;
+                default:
+                    System.out.println("User option " + userOption + " is not implemented.");
+                    break;
+            }
+        } while (userOption != UserOption.EXIT);
+
+        SessionManager.shutDown();
     }
 }
